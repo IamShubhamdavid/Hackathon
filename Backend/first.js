0977e736e97d5fa65ca5aed3271cdece82ac1fc3
Shubham kumar <shubhamdavid4606@gmail.com>
2025-08-21 20:39:57 +0530

    Backend

---
+++ Backend/first.js
@@ -0,0 +1,20 @@
+// const bcrypt=require("bcrypt")
+
+// // Hashing SHA256 we use for storing the password but not always
+// // Rainbow Table 
+// // Hmlog hashing table mein salt add krte just like add some strings
+        
+
+
+// const password="Rohit@123";
+
+// async function Hashing(){
+
+// const hashpass= await bcrypt.hash(password,10);
+
+// const ans=await bcrypt.compare(password,hashpass);
+// console.log(ans);
+
+// }
+
+// Hashing();
